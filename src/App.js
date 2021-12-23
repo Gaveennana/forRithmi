454798cf95d47683985aee17fff423a9761c8275
Gaveen Nanayakkara <gaveen1999@gmail.com>
2021-12-23 18:40:10 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello beautiful");
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setHeadingText("I love you more than anything Rithmi <3");
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{headingText}</h1>
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onMouseOut={handleMouseOut}
+        onMouseOver={handleMouseOver}
+        onClick={handleClick}
+      >
+        Click for a surprise
+      </button>
     </div>
   );
 }
-
 export default App;
